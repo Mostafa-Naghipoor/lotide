66a46aa9a1d379e8387478de0f760721163b7d96
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-17 14:53:03 -0800

    edited

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,27 @@
+// to check the equality of two parameters
 const assertEqual = function(actual, expected) {
     if (actual === expected) {
       console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
     } else {
       console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
     }
-  };
-
-  const eqArrays = function(array1, array2) {
-    for( let i = 0; i < array1.length; i++){
-        if(array1[i]=== array2[i]){
-            return true;
-
-        }else {
-            return false;
-        }
+};
+// to compare the equality of two arrays
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i]!==array2[i]){
+        return false;
     }
   }
-  const assertArraysEqual = function(array1, array2){
-    console.log(`The result is ${eqArrays()}`);
-  }
-  
-  // TEST CODE
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  return true;
+}
+
+// using eqArray() and assertEqual() we get the final comparison result
+const assertArraysEqual = function(array1, array2){
+  const result = eqArrays(array1, array2);
+  assertEqual(result, true);
+}
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual([1, 2,3],[0,2,3]);
