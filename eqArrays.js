3958726b9b02de3afe60447c687de38be2e6f880
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-17 00:51:51 -0800

    function to eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if(actual=== expected){
+        console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    }else{
+        console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+    }
+};
+const eqArrays = function(array1, array2) {
+    for (let i = 0; i < array1.length; i++){
+        if (array1[i]===array2[i]){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
