59b03ffd529f8daa9871c5c21fc3e0f2f95a9b0a
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-17 16:12:22 -0800

    added middle function

---
+++ middle.js
@@ -0,0 +1,39 @@
+// to compare the equality of two arrays
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++){
+    if (array1[i]!==array2[i]){
+        return false;
+    }
+  }
+  return true;
+}
+
+const middle = function(array) {
+     if(array.length > 2 ){
+        if(array.length % 2 === 1 ){
+            return [array[Math.floor(array.length / 2)]]; 
+        } else if(array.length % 2 === 0){
+            return [array[array.length / 2 - 1], array[array.length / 2]];
+        }  
+        
+    } else{
+        return [];
+    }
+
+}
+
+
+
+// if(array.length < 2 ) {
+//     return [];
+// } else if(array.length % 2 === 1 ){
+//     return [array[Math.floor(array.length / 2)]]; 
+// } else if(array.length % 2 === 0){
+//     return [array[array.length / 2 - 1], array[array.length / 2]];
+
+// }  
+
+
+console.log(middle([1,2]));
+console.log(middle([1,2,3,4,5]));
+console.log(middle([1,2,3,4,5,6]));
