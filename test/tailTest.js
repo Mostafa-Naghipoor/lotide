c10a8e83a847bf06dda00e4dba0b8770554d89d9
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-21 23:42:30 -0800

    required modules

---
+++ test/tailTest.js
@@ -0,0 +1,7 @@
+const tail = require("../tail");
+const assertEqual = require("../assertEqual");
+const X = tail([5,6,7]);
+const Y = [6,7];
+for(let i = 0; i < X.length; i++){
+ assertEqual(X[i],Y[i]);
+}
