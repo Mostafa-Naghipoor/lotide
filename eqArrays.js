d187f17b7ba6defaeccca95407ef1e891fbf31a9
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-22 00:24:18 -0800

    added modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,15 @@
-const eqArrays = function(array1, array2) {
-    for (let i = 0; i < array1.length; i++){
-        if (array1[i]!==array2[i]){
-            return false;
+
+const eqArrays = function (actual, expected) {
+    if (actual.length !== expected.length) {
+      return false;
+    } else {
+      for (let i = 0; i < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+          return false;
         }
+      }
+      return true;
     }
-    return true;
-}
-module.exports = eqArrays;
+  };
+  
+  module.exports = eqArrays;
