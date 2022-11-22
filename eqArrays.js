52b16d2dfc2e8ff5c4d4f60e0c5d689575724bdf
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-21 23:57:42 -0800

    exported modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-    if(actual=== expected){
-        console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-    }else{
-        console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-    }
-};
 const eqArrays = function(array1, array2) {
     for (let i = 0; i < array1.length; i++){
         if (array1[i]!==array2[i]){
@@ -14,5 +6,4 @@ const eqArrays = function(array1, array2) {
     }
     return true;
 }
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
