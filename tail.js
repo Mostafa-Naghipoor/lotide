dfb9fc33a23aedcafd130fb5c9cb154597d2648e
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-21 23:44:32 -0800

    added modules

--- tail.js
+++ tail.js
@@ -1,21 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-// let tail = require('lodash.tail');
 const tail = function(array){
   return array.slice(1);
 }
+module.exports = tail;
 
-// TEST CODE
-
-const X = tail([5,6,7]);
-const Y = [6,7];
-for(let i = 0; i < X.length; i++){
- assertEqual(X[i], Y[i]);
-}
