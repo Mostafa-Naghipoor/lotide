267062f4b2452896a3928cffa87797b001204eae
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-11-22 16:09:00 -0800

    final version

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,17 @@
+const assert = require("chai").assert;
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
-const X = tail([5,6,7]);
-const Y = [6,7];
-for(let i = 0; i < X.length; i++){
- assertEqual(X[i],Y[i]);
-}
+
+let testArr = ["Hello", "Lighthouse", "Labs"];
+
+describe("#tail", () => {
+  it("returns 3 from ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(testArr.length, 3);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] from ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), [
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+});
